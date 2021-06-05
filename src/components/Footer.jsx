10c7cf2eb5e0d54749f491173e41431d8bc349db
Sneha Shinde <snehashinde9145@gmail.com>
2021-06-05 20:25:22 +0530

    Add files via upload

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+  var curr_year = new Date().getFullYear();
+
+  return (
+    <footer>
+      <p>Copyright @ {curr_year}</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/components/Note.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>JavaScript & React.js</h1>
+      <p>
+        This was an amazing bootcamp taken be Shaurta Sinha Sir. We Coverd
+        everything from Scrach
+      </p>
+    </div>
+  );
+}
+
+export default Note;
